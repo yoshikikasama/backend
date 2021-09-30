c3a95d0937997c4a9da6af99cbce4b875c07bb69
yoshikikasama <kasamayoshiki@gmail.com>
2021-09-30 22:35:21 +0900

    environment.py revised

--- environments.py
+++ environments.py
@@ -2,6 +2,6 @@ import os
 
 
 def get_env():
-    os.environ["SENDGRID_API_KEY"] = os.environ.get("SENDGRID_API_KEY", "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")
+    os.environ["SENDGRID_API_KEY"] = os.environ.get("SENDGRID_API_KEY", "test")
     return os.environ
 
