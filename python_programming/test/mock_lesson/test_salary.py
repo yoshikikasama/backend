a6be7476ff6168ad724d51783850d2a059efbd08
yoshikikasama <kasamayoshiki@gmail.com>
2021-10-27 07:43:12 +0900

    20211027

--- python_programming/test/mock_lesson/test_salary.py
+++ python_programming/test/mock_lesson/test_salary.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
+from unittest import mock
 import salary
 
 class TestSalary(unittest.TestCase):
@@ -16,7 +17,43 @@ class TestSalary(unittest.TestCase):
         s.bonus_api.bonus_price = MagicMock(return_value=0)
         self.assertEqual(s.calculation_salary(), 100)
         s.bonus_api.bonus_price.assert_not_called()
+    @mock.patch('salary.ThirdPartyBonusRestApi.bonus_price', return_value=1)
+    def test_calculation_salary_patch(self, mock_bonus):
+        # mock_bonus.return_value = 1
 
+        s = salary.Salary(year=2017)
+        salary_price = s.calculation_salary()
+
+        self.assertEqual(salary_price, 101)
+        mock_bonus.assert_called()
+
+    def test_calculation_salary_patch_with(self):
+        with mock.patch(
+            'salary.ThirdPartyBonusRestApi.bonus_price'
+        ) as mock_bonus:
+            mock_bonus.return_value = 1
+
+            s = salary.Salary(year=2017)
+            salary_price = s.calculation_salary()
+
+            self.assertEqual(salary_price, 101)
+            mock_bonus.assert_called()
+
+    def setUp(self):
+        self.patcher = mock.patch('salary.ThirdPartyBonusRestApi.bonus_price')
+        self.mock_bonus = self.patcher.start()
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def test_calculation_salary_patcher(self):
+        self.mock_bonus.return_value = 1
+
+        s = salary.Salary(year=2017)
+        salary_price = s.calculation_salary()
+
+        self.assertEqual(salary_price, 101)
+        self.mock_bonus.assert_called()
 
 if __name__ == '__main__':
     unittest.main()
