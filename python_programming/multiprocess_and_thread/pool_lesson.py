12f98314124a1451ee5090abe2e01d3db0998f30
yoshikikasama <kasamayoshiki@gmail.com>
2021-10-30 16:26:38 +0900

    20211030

---
+++ python_programming/multiprocess_and_thread/pool_lesson.py
@@ -0,0 +1,24 @@
+import multiprocessing
+import logging
+import time
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(processName)s: %(message)s'
+)
+
+
+def worker1(i):
+    logging.debug('start')
+    time.sleep(3)
+    logging.debug('end')
+    return i
+
+
+if __name__ == '__main__':
+    # pool:同時にrunするthreadの数を指定できる
+    with multiprocessing.Pool(1) as p:
+        p1 = p.apply_async(worker1, (100, ))
+        p2 = p.apply_async(worker1, (100, )) 
+        logging.debug('executed')
+        logging.debug(p1.get())
+        logging.debug(p2.get())
