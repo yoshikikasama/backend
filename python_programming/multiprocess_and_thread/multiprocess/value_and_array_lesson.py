2fdc118da46152fd7c4c26e27b1b62c06cb9e646
yoshikikasama <kasamayoshiki@gmail.com>
2021-11-02 07:51:01 +0900

    20211102

---
+++ python_programming/multiprocess_and_thread/multiprocess/value_and_array_lesson.py
@@ -0,0 +1,28 @@
+import logging
+import multiprocessing
+import time
+from typing import MutableMapping
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(processName)s: %(message)s'
+)
+
+def f(num, arr):
+    logging.debug(num)
+    num.value += 1
+    logging.debug(arr)
+    for i in range(len(arr)):
+        arr[i] *= 2
+
+if __name__ == '__main__':
+    num = multiprocessing.Value('f', 0.0)
+    arr = multiprocessing.Array('i', [1, 2 ,3 ,4 ,5])
+
+    p = multiprocessing.Process(target=f, args=(num, arr))
+    p.start()
+    p.join()
+    p2 = multiprocessing.Process(target=f, args=(num, arr))
+    p2.start()
+    p2.join()
+    logging.debug(num.value)
+    logging.debug(arr[:])
