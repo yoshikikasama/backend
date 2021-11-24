a34dc5b23e57d438cd021bc255d46cd2c2f2b24d
yoshikikasama <kasamayoshiki@gmail.com>
2021-11-24 22:34:19 +0900

    20211124

---
+++ python_programming/useful_packages/format_lesson.py
@@ -0,0 +1,4 @@
+print('{name:{fill}{align}{width}}'.format(name='center', fill='*', align='^', width=30))
+print('{:,}'.format(123456789))
+print('{:+f}{:+f}'.format(3.14, -3.14))
+print('{:.2%}'.format(19/22))
