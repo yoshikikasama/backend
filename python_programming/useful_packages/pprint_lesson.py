6d6f60572a0445fc49f84f81182729b876fac6ad
yoshikikasama <kasamayoshiki@gmail.com>
2021-11-28 08:41:54 +0900

    20211128

--- python_programming/useful_packages/pprint_lesson.py
+++ python_programming/useful_packages/pprint_lesson.py
@@ -7,5 +7,5 @@ l.insert(0, l[:])
 
 pp = pprint.PrettyPrinter(indent=4, width=40)
 # pp.pprint(l)
-d = {'a': 'A', 'b': 'B', 'c': {'x':{'y':'Y'}}}
+d = {'a': 'A', 'b': 'B', 'c': {'x': {'y': 'Y'}}}
 print(json.dumps(d, indent=4))
