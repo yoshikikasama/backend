823a6299097822d8266cd02617e6e697f5f3c88c
yoshikikasama <kasamayoshiki@gmail.com>
2021-11-28 08:41:12 +0900

    20211128

--- python_programming/useful_packages/pprint_lesson.py
+++ python_programming/useful_packages/pprint_lesson.py
@@ -3,3 +3,9 @@ import pprint
 
 l = ['apple', 'orange', 'banana', 'peach', 'mango']
 
+l.insert(0, l[:])
+
+pp = pprint.PrettyPrinter(indent=4, width=40)
+# pp.pprint(l)
+d = {'a': 'A', 'b': 'B', 'c': {'x':{'y':'Y'}}}
+print(json.dumps(d, indent=4))
