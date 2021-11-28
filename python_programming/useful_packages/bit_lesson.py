f630a6e0e6c046c285e9879ef25c4e435d9f1902
yoshikikasama <kasamayoshiki@gmail.com>
2021-11-29 07:35:18 +0900

    20211129

---
+++ python_programming/useful_packages/bit_lesson.py
@@ -0,0 +1,30 @@
+# 論理和
+print(0 | 0)
+print(0 | 1)
+print(1 | 0)
+print(1 | 1)
+# 論理積
+print('論理積')
+print(0 & 0)
+print(0 & 1)
+print(1 & 0)
+print(1 & 1)
+
+# 排他的論理和
+print('排他的論理積')
+print(0 ^ 0)
+print(0 ^ 1)
+print(1 ^ 0)
+print(1 ^ 1)
+# 反転
+print('反転')
+print(bin(0))
+print(bin(~0))
+print(bin(1))
+print(bin(~1))
+# シフト
+print('シフト')
+print(bin(1 << 0))
+print(bin(1 << 1))
+print(bin(1 << 2))
+print(bin(5 << 1))
