4fc7ea63e74d9effe486fe29e96a562a60d732a5
yoshikikasama <kasamayoshiki@gmail.com>
2021-12-04 08:14:06 +0900

    20211204

--- python_programming/useful_packages/memoize_lesson.py
+++ python_programming/useful_packages/memoize_lesson.py
@@ -1,15 +1,22 @@
-def memoize(f):
-    memo = {}
+# def memoize(f):
+#     memo = {}
 
-    def _wrapper(n):
-        if n not in memo:
-            # function実行
-            memo[n] = f(n)
-        return memo[n]
-    return _wrapper
+#     def _wrapper(n):
+#         if n not in memo:
+#             # function実行
+#             memo[n] = f(n)
+#             print('hit')
+#             print(memo)
+#         return memo[n]
+    # return _wrapper
 
 
-@memoize
+# @memoize
+import functools
+# 何度も値を取得するようなapiの結果を保持するときなどに使用する
+
+
+@functools.lru_cache(maxsize=5)
 def long_func(n):
     r = 0
     for i in range(10000000):
@@ -20,6 +27,10 @@ def long_func(n):
 for i in range(10):
     print(long_func(i))
 
+print(long_func.cache_info())
 print('next run')
 for i in range(10):
     print(long_func(i))
+
+print(long_func.cache_info())
+long_func.cache_clear()
