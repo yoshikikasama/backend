c69f49db96b3073ce6196e1c9fec12ce20550367
yoshikikasama <kasamayoshiki@gmail.com>
2021-12-30 14:32:07 +0900

    20211230

--- python-study/codility/oddoccurrencesInArray.py
+++ python-study/codility/oddoccurrencesInArray.py
@@ -5,20 +5,33 @@
 
 # pythonのset関数を使用すれば、簡単にできるかも
 
-def solution(A):
-    test_list = []
-    for i in A:
-        print(i)
-        if i in test_list:
-            for j in range(len(test_list)):
-                print(test_list)
-                if i == test_list[j]:
-                    test_list.pop(j)
-                    break
-        else:
-            test_list.append(i)
+# def solution(A):
+#     test_list = []
+#     for i in A:
+#         print(i)
+#         if i in test_list:
+#             for j in range(len(test_list)):
+#                 print(test_list)
+#                 if i == test_list[j]:
+#                     test_list.pop(j)
+#                     break
+#         else:
+#             test_list.append(i)
         
-    return test_list[0]
+    # return test_list[0]
+
+# set関数では重複した要素は取り除くことができる
+def solution(A):
+    numSet = set()
+    for i in A:
+        if i in numSet:
+            numSet.remove(i)
+        else:
+            numSet.add(i)
+
+    for j in numSet:
+        return j
+
 
 A = [9, 3, 9, 3, 9, 7, 9]
 sol = solution(A)
