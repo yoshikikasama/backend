b3f554492021bb710f769434071a7ff153687e30
yoshikikasama <kasamayoshiki@gmail.com>
2022-01-09 16:04:53 +0900

    20220109

---
+++ python_programming/asynico/nativeroutin.py
@@ -0,0 +1,17 @@
+import asyncio
+# 非同期処理
+# loopのobjectを作成
+loop = asyncio.get_event_loop()
+
+async def worker():
+    print('start')
+    # 非同期用のコルーチンのsleep
+    # yield from asyncio.sleep(2)
+    await asyncio.sleep(2)
+    print('stop')
+
+
+if __name__ == '__main__':
+    # 終わるまで待つ
+    loop.run_until_complete(asyncio.wait([worker(), worker()]))
+    loop.close()
