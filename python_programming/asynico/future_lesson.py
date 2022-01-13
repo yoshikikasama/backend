8b72b2f4ae39e19b60918e1bd65015fe2aec7290
yoshikikasama <kasamayoshiki@gmail.com>
2022-01-14 07:47:16 +0900

    20220114

--- python_programming/asynico/future_lesson.py
+++ python_programming/asynico/future_lesson.py
@@ -7,8 +7,19 @@ async def f(future):
     await asyncio.sleep(1)
     future.set_result('Future is done!')
 
+
+def got_result(future):
+    print(future.result())
+    loop.stop()
+
+
 future = asyncio.Future()
+# 確実に終わることを確認するメソッド
 asyncio.ensure_future(f(future))
-loop.run_until_complete(future)
-print(future.result())
+
+future.add_done_callback(got_result)
+
+loop.run_forever()
+# loop.run_until_complete(future)
+# print(future.result())
 loop.close()
