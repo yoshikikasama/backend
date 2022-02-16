8d3d5d13f9b0ca2b00df63da0b3df8549b22e3a2
yoshikikasama <kasamayoshiki@gmail.com>
2022-02-16 22:31:40 +0900

    20220216

--- data_analytics/boto3/tutorials/csv_from_s3.py
+++ data_analytics/boto3/tutorials/csv_from_s3.py
@@ -6,9 +6,32 @@ aws configure
 
 boto3 Client APIとResource API
 Client API・・・リソースを操作する場合も参照系と同様に、対象のリソースIDを引数に加えてメソッドを実行する。
+ex)
+s3 = boto3.client('s3')
+# バケット一覧を取得
+s3.list_buckets()
+obj = client.get_object(Bucket='test_bucket', Key='test.text')
+print(obj['body'].read())
+
 Resource API・・・リソースを操作する場合には対象のリソースを「object」として取得してからobjectのもつメソッドを呼び出して操作する。
+                 よりオブジェクト指向っぽく書けるよう用意された高レベルなライブラリです。
+                 必ずしもすべてのサービスで用意されているわけではなくて、S3 などの頻出サービスでのみ利用できます。
+                 目的に応じて resource が使えないか検討してみるとよいでしょう。
+ex)
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('test_bucket')
+obj = bucket.Object('test.text').get()
+print(obj['body'].read())
+
+s3_resource = boto3.resource('s3')
+clientも取得可能
+s3_client = s3_resource.meta.client
+
 BytesIO・・・メモリ上でバイナリデータを扱うための機能です。Python の標準ライブラリ io に含まれています。バイナリデータとは主に画像や音声などのデータのことです。コンピューターで扱うデータは全てバイナリデータなのですが、テキストデータと対比して用いられます。
 TextIOWrapper・・・テキストモード
+クラスメソッド・・・インスタンスを作らずともアクセスできる。クラスの属性を触りたい時に使う。
+インスタンスメソッド・・・インスタンスからアクセスする。インスタンスの属性をさわりたいとき。
+
 
 1.　s3　bucketの作成
 2.　「test.csv」のupload
@@ -28,6 +51,10 @@ KEY = "test.csv"
 
 def s3_client():
     s3 = boto3.client('s3')
+    # <botocore.client.S3 object at 0x7fb16d527250>
+    # <class 'botocore.client.S3'>
+    # s3.ServiceResource()
+    # <class 'boto3.resources.factory.s3.ServiceResource'>
     return s3
 
 
