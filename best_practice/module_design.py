1443b16ee8e415cbcf8f8d373d0ba7e2269f0230
yoshikikasama <kasamayoshiki@gmail.com>
2022-03-14 22:53:31 +0900

    20220314

--- best_practice/module_design.py
+++ best_practice/module_design.py
@@ -10,7 +10,7 @@
 ■モジュールのまとめ方
 ・意味でまとめられる時は積極的に分割する。
 ・モジュールが大きくなった場合は、パッケージ(__init__.pyのあるディレクトリ)にまとめる。
-
+ex)
 .
 ├── api # 外部APIにアクセスする処理をまとめる
 │   ├── __init__.py
@@ -20,11 +20,16 @@
 │   ├── __init__.py
 │   ├── list.py # 商品の一覧を表示するコマンドの入出力を扱う処理をまとめる
 │   └── purchase.py # 商品を購入するコマンドの入出力を扱う処理をまとめる
-├── consts.py # バックエンドAPIのホストなど定数をまとめる
+├── consts.py # バックエンドAPIのホストなど定数をまとめる(もしくはconstants.py)
 ├── main.py # ツールのエントリーポイントのmain関数を置く 
 ├── models.py # 商品やユーザーのデータを永続化するクラスをまとめる
 ├── purchase.py # 商品を購入する処理をまとめる
 └── validators.py # コマンドラインからの入力をチェックする処理をまとめる
 
+■使いやすいモジュール名
+認証：　authentication.py
+認可、パーミッション： permission.py / authorization.py
+バリデーション：　validations.py / validators.py
+例外： exceptions.py
 
 """
