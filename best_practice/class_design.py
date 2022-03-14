144fa063b865b3e4cd1303ffd26e921ab9cd500a
yoshikikasama <kasamayoshiki@gmail.com>
2022-03-14 22:28:49 +0900

    20220314

--- best_practice/class_design.py
+++ best_practice/class_design.py
@@ -1,19 +1,39 @@
 """クラス設計
 
+特定のキーを持つ辞書のデメリット
+・特定のキーが存在しているかのcheckが必要になることがある。
+・他の形式の辞書で使えない関数なので再利用性が低い。
 
+クラスのメリット
+・REPL(対話型実行環境)でインスタンスを表示するときに、クラス名が表示されるのでわかりやすい。
+・isinstanceを使用してクラスのインスタンスであるかをcheckできる。
+・型アノテーションをするときに指定したclassが引数に渡されるかをチェックできる。
+・IDEで動的解析をするときにクラスの定義元にジャンプしたり、関数の入出力をクラスのインスタンスで制限できる。
+・クラス名でコードを検索すれば、そのクラスが使われている処理をすぐに見つけられる。
+・目安は特定のkeyを持つ辞書を使う関数が増えたときにクラスにする程度でいい。
+
+■initでのデフォルト引数の初期化は引数が多いと冗長になるためinitではなく、dataclassを使用する。
+
+■「事前に他のメソッドを呼び出して値を設定する必要がある」という設計は、値が変更したときに再度呼び出す必要があるため
+　よくない。
 """
 # クラス設計
 # 特定のキーを持つ辞書を期待するならクラスを定義する
 import json
 from dataclasses import dataclass
 from datetime import date
+from .dataapi import retrieve_product_detail
 
 
 @dataclass
 class User:
+    username: str
+    email: str
     last_name: str
     first_name: str
     birthday: date
+    role: str
+    mail_confirmed: bool = False
 
     # クラスにすることで、それぞれの処理をメソッドやプロパティーとして実装できる。
     # user.fullnameのように簡潔にプログラムを書ける
@@ -23,6 +43,8 @@ class User:
 
     @property
     def age(self):
+        # birthdayが変更される可能性があるため、age関数として作成
+        # functool.lru_cache
         today = date.today()
         born = self.birthday
         age = today.year - born.year
@@ -35,3 +57,24 @@ class User:
 def load_user():
     with open('./user.json', encoding='utf-8') as f:
         return User(**json.load(f))
+
+
+# クラスメソッド
+@dataclass
+class Product:
+    id: int
+    name: str
+
+    @classmethod
+    def retrieve(cls, id: int):
+        """データAPIから商品の情報を取得して、インスタンスとして返す
+        """
+        data = retrieve_product_detail(id)
+        return cls(
+            id=data['id'],
+            name=data['name']
+        )
+
+
+# クラスメソッドにすることでProductクラスから値を取得する処理も使用できる。
+product = Product.retrieve(1)
---
+++ best_practice/dataapi.py
@@ -0,0 +1,9 @@
+
+from urllib import request
+
+import requests
+
+
+def retrieve_product_detail(id):
+    res = requests.get(f'/api/products/{id}')
+    return res.json()
