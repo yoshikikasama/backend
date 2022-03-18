3273d2aa8176abe5acdaddf011822cae23c0c726
yoshikikasama <kasamayoshiki@gmail.com>
2022-03-18 22:56:13 +0900

    20220318

--- best_practice/unittest/sales.py
+++ best_practice/unittest/sales.py
@@ -1,5 +1,68 @@
 import csv
+from dataclasses import dataclass
+from typing import List
+
+# def load_sales(sales_path='./sales.csv'):
+#     sales = []
+#     with open(sales_path, encoding='utf-8') as f:
+#         for sale in csv.DictReader(f):
+#             # 値の変換
+#             try:
+#                 sale['price'] = int(sale['price'])
+#                 sale['amount'] = int(sale['amount'])
+#             except (ValueError, TypeError, KeyError):
+#                 continue
+#             # 値のチェック
+#             if sale['price'] <= 0:
+#                 continue
+#             if sale['amount'] <= 0:
+#                 continue
+#     # 売上の計算
+#     sum_price = 0
+#     for sale in sales:
+#         sum_price += sale['amount'] * sale['price']
+#     return sum_price, sales
 
 
-def load_sales(sales_path='./sales.csv'):
-    sales = []
+# 売上(CSVの各行)を表すクラスに分離する
+@dataclass
+class Sale:
+    id: int
+    item_id: int
+    price: int
+    amount: int
+
+    def validate(self):
+        if sale['price'] <= 0:
+            raise ValueError('Invalid sale.price')
+        if sale['ammount'] <= 0:
+            raise ValueError('Invalid sale.amount')
+
+    # 各売上の料金を計算する処理をSalesに実装
+    # property:値を簡単に変更できない
+    @property
+    def price(self):
+        return self.amount * self.price
+
+
+@dataclass
+class Sales:
+    data: List[Sale]
+
+    @property
+    def price(self):
+        return sum(sale.price for sale in self.data)
+
+    @classmethod
+    def from_assert(cls, path='./sales.csv'):
+        data = []
+        with open(path, encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                try:
+                    sale = Sale(**row)
+                    sale.validate()
+                except Exception:
+                    continue
+                data.append(sale)
+        return cls(data=data)
--- best_practice/unittest/test_unittest_design.py
+++ best_practice/unittest/test_unittest_design.py
@@ -11,7 +11,11 @@
 ・処理を分離して全ての動作確認に全てのデータが必要ないようにする
 ・関数やクラスを分離して、細かいテストは分離した関数、クラスを対象に行う
 　（分離した関数を呼び出す関数では細かいテストは書かないようにする。)
+
+fixture(conftest)をうまく活用する。
+parametrizeをうまく活用する。
 """
+from best_practice.unittest.sales import load_sales
 import main
 import pytest
 
@@ -69,3 +73,27 @@ class TestSignupAPIView:
         }
         assert expected == actual
 
+class TestLoadSales:
+    def test_invalid_row(self, tmpdir):
+        test_file = tmpdir.join('test.csv')
+        test_file.write("""id,item_id,price
+        1,1,100
+        2,1,100
+        """)
+        
+        sum_price, actual_sales = load_sales(test_file.strpath)
+        assert sum_price == 0
+        assert len(actual_sales) == 0
+
+    def test_invalid_type_amount(self, tmpdir):
+        # テストのたびにCSVファイルを毎度用意する必要がある。
+        test_file = tmpdir.join('test.csv')
+        test_file.write("""id,item_id,price,amount
+        1,1,100,foobar
+        2,1,100,2
+        """)
+        sum_price, actual_sales = load_sales(test_file.strpath)
+        assert sum_price == 400
+        assert len(actual_sales) == 1
+
+        
