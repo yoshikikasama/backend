8a26f79eec2a296506b9401f0a3b3cac05899c05
yoshikikasama <kasamayoshiki@gmail.com>
2022-04-10 13:22:12 +0900

    20220410

---
+++ framework/fastapi/full_stack_web_dev/core/config.py
@@ -0,0 +1,6 @@
+class Settings:
+    PROJECT_TITLE: str = "full_stack_web_dev"
+    PROJECT_VERSION: str = "0.1.1"
+
+
+settings = Settings()
---
+++ framework/fastapi/full_stack_web_dev/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from core.config import settings
+
+app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+
+@app.get("/")
+def hello_api():
+    return {"detail": "Hello World!"}
+
+# uvicorn main:app --reload
+# /docs
