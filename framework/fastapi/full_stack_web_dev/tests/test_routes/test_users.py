6cd327c56a51a642fe0ba89d239c372a89cdbfa0
yoshikikasama <kasamayoshiki@gmail.com>
2022-04-12 22:38:07 +0900

    20220412

---
+++ framework/fastapi/full_stack_web_dev/tests/test_routes/test_users.py
@@ -0,0 +1,13 @@
+import json
+
+
+def test_create_user(client):
+    data = {
+        "username": "testusername",
+        "email": "abc@test.com",
+        "password": "123456",
+    }
+    response = client.post("/users/", json.dumps(data))
+    assert response.status_code == 200
+    assert response.json()["email"] == "abc@test.com"
+    assert response.json()["is_active"] is True
