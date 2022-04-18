3755f5919e4bb5eba632f3a2de33b00cb692fcf4
yoshikikasama <kasamayoshiki@gmail.com>
2022-04-19 06:48:42 +0900

    20220419

--- framework/fastapi/full_stack_web_dev/core/config.py
+++ framework/fastapi/full_stack_web_dev/core/config.py
@@ -17,7 +17,7 @@ class Settings:
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-    SECRET_KEY: str = os.get_env("SECRET_KEY")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
