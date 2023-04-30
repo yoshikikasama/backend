c16c625e677b4d02ca41679088c167da79064aff
yoshikikasama <kasamayoshiki@gmail.com>
2023-04-30 17:55:43 +0900

    20230430

--- framework/streamlit/image_exam.py
+++ framework/streamlit/image_exam.py
@@ -1,3 +1,4 @@
+import io
 import os
 import requests
 import streamlit as st
@@ -9,27 +10,30 @@ SUBSCCRIPTION_KEY = os.environ["SUBSCCRIPTION_KEY"]
 assert SUBSCCRIPTION_KEY
 
 face_api_url = "<face_api_url>"
-img = Image.open("sample.jpg")
-with open("sample.jpg", "rb") as f:
-    binary_img = f.read()
+# img = Image.open("sample.jpg")
 
 
-headers = {"Content-Type": "application/octet_stream", "Ocp-Apim-Subscription-Key": SUBSCCRIPTION_KEY}
+uploaded_file = st.file_uploader("Chose an image...", type="png")
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
 
-params = {
-    "returnFaceId": "true",
-    "returnFaceAttributes": "age, gender,headPose, smile, facialHair, glasses, emotion, hair,makeup, occulusion",
-}
+    with io.BytesIO() as output:
+        img.save(output, format="JPEG")
+    binary_img = output.getvalue()  ## バイナリ取得
+    headers = {"Content-Type": "application/octet_stream", "Ocp-Apim-Subscription-Key": SUBSCCRIPTION_KEY}
 
-response = requests.post(face_api_url, params=params, headers=headers, data=binary_img)
-result = response.json()
-result
+    params = {
+        "returnFaceId": "true",
+        "returnFaceAttributes": "age, gender,headPose, smile, facialHair, glasses, emotion, hair,makeup, occulusion",
+    }
 
-draw = ImageDraw.Draw(img)
-draw.line([(0, 50), (200, 50), (0, 150), (200, 150)], fill="red", width=5)
-st.image(img, caption="test Image", use_column_width=True)
+    response = requests.post(face_api_url, params=params, headers=headers, data=binary_img)
+    result = response.json()
+    result
 
-# uploaded_file = st.file_uploader("Chose an image...", type="png")
-# if uploaded_file is not None:
-#     img = Image.open(uploaded_file)
-#     st.image(img, caption="Uploaded Image.", use_column_width=True)
+    draw = ImageDraw.Draw(img)
+    draw.line([(0, 50), (200, 50), (0, 150), (200, 150)], fill="red", width=5)
+    st.image(img, caption="test Image", use_column_width=True)
+
+# with open("sample.jpg", "rb") as f:
+#     binary_img = f.read()
