9970be07bf90ab214a4fc6d25b202b422d57b0bb
yoshikikasama <kasamayoshiki@gmail.com>
2023-05-23 07:57:40 +0900

    20230523

--- other/OCR/README.md
+++ other/OCR/README.md
@@ -2,22 +2,6 @@
 
 ## Azure OCR
 
-RES_GROUP=<resourcegroup-name>
-ACCT_NAME=<computervision-account-name>
-
-export ACCOUNT_REGION=$(az cognitiveservices account show \
- --resource-group $RES_GROUP \
- --name $ACCT_NAME \
- --query location \
- --output tsv)
-
-export ACCOUNT_KEY=$(az cognitiveservices account keys list \
- --resource-group $RES_GROUP \
- --name $ACCT_NAME \
- --query key1 \
- --output tsv)
-
-export ACCOUNT_REGION=japaneast
 export ACCOUNT_KEY=${ACCOUNT_KEY}
 export END_POINT=${END_POINT}
 
--- other/OCR/azure/ocr_test.py
+++ other/OCR/azure/ocr_test.py
@@ -1,29 +1,47 @@
 import os
 import time
-import requests
+
+# import requests
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import ComputerVisionOcrErrorException
 
 
 key = os.environ["ACCOUNT_KEY"]
 endpoint = os.environ["END_POINT"]
-image_path = "../img/Screenshot 2023-05-20 at 7.40.54.png"  # ローカルの画像パス
+image_path = "../img/Screenshot 2023-05-23 at 7.55.37.png"  # ローカルの画像パス
 
 
 def main(image_path):
     computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(key))
-    with open(image_path, "rb") as file:
-        try:
-            recognize_handw_results = computervision_client.read_in_stream(file, language="ja", raw=True)
-        except ComputerVisionOcrErrorException as e:
-            print("errors:", e.response)
-            print(vars(e))
-            raise e
-        time.sleep(5)
-        result_url = recognize_handw_results.headers.get("Operation-Location")
-        result = requests.get(result_url, headers={"Ocp-Apim-Subscription-Key": key})
-        print(result.text)
+    local_image = open(image_path, "rb")
+    try:
+        recognize_results = computervision_client.read_in_stream(local_image, language="ja", raw=True)
+    except ComputerVisionOcrErrorException as e:
+        print("errors:", e.response)
+        print(vars(e))
+        raise e
+    # 結果を取得するための操作IDを取得
+    operation_location_remote = recognize_results.headers["Operation-Location"]
+    operation_id = operation_location_remote.split("/")[-1]
+
+    # 結果が利用可能になるまで待つ
+    while True:
+        get_text_results = computervision_client.get_read_result(operation_id)
+        if get_text_results.status not in ["notStarted", "running"]:
+            break
+        time.sleep(1)
+
+    # テキストの出力
+    if get_text_results.status == OperationStatusCodes.succeeded:
+        with open("output.txt", "w", encoding="utf-8") as f:
+            for text_result in get_text_results.analyze_result.read_results:
+                for line in text_result.lines:
+                    f.write(line.text + "\n")
+
+
+print("Texts are written to output.txt")
 
 
 if __name__ == "__main__":
